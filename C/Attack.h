c78cbda75bb555b499f008e5c07f379f14bc9794
Lior Vinman <liorvi35@gmail.com>
2023-04-29 20:29:54 +0300

    Update Attack.h

--- C/Attack.h
+++ C/Attack.h
@@ -1,29 +1,24 @@
-#ifndef ATTACK_H
-#define ATTACK_H
+#ifndef _ATTACK_H_
+#define _ATTACK_H_
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <time.h>
-#include <sys/time.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
-#include <netinet/ether.h>
-#include <arpa/inet.h>
-#include <errno.h>
 
-#define S_IP "10.9.0.2"
-#define S_PORT 12345
-#define D_IP "10.9.0.3"
-#define D_PORT 80
-#define NUM_ITERATIONS 100
+#define RESULTS_FILE "syns_results_c.txt"
+#define TARGET_IP_ADDR "10.9.0.3"
+#define TARGET_PORT 80
 #define NUM_PACKETS 10000
-#define SYN_TIMES_FILE "syns_results_c.txt"
+#define NUM_ITERATIONS 100
 
-void set_ip_layer(struct iphdr*);
-void set_tcp_layer(struct tcphdr*);
+void set_ip_layer(struct sockaddr_in*, struct iphdr*, int, FILE*);
+void set_tcp_layer(struct sockaddr_in*, struct iphdr*, struct tcphdr*);
+
+uint16_t calculate_ip_checksum(struct iphdr*);
+uint16_t calculate_tcp_checksum(struct iphdr*, struct tcphdr*);
+
+
+int get_random_port();
+char* get_random_ipv4(int, FILE*);
 
 #endif
